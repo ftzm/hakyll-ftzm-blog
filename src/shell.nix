52fdac210ff2524ff70deda413d795c30acc1d6a
ftzm <matthew@fitzsimmons.org>
2019-06-28 05:31:00 -0400

    add dhall-json to shell.nix

--- src/shell.nix
+++ src/shell.nix
@@ -1,9 +1,18 @@
 { pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
 
+let dhall_latest =  import <dhall> {};
+in
+
 pkgs.haskell.lib.buildStackProject {
   name = "ftzm-blog";
   inherit ghc;
-  buildInputs = with pkgs; [ zlib unzip sass inotify-tools];
+  buildInputs = with pkgs; [
+    zlib
+    unzip
+    sass
+    inotify-tools
+    dhall_latest.linux-dhall-json
+  ];
   LANG = "en_US.UTF-8";
   TMPDIR = "/tmp";
 }
